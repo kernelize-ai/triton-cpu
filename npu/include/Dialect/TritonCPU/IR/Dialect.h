1a4c10ebb13aa9cff0942f56f2cff85e81b9b2fd
Alex Baden <alex@kernelize.ai>
2025-08-15 13:31:35 -0400

    Cleanup dependencies and includes for TritonCPU dialect/pass

--- npu/include/Dialect/TritonCPU/IR/Dialect.h
+++ npu/include/Dialect/TritonCPU/IR/Dialect.h
@@ -6,10 +6,10 @@
 #include "mlir/IR/Dialect.h"
 
 // TritonCPU depends on Triton
-#include "npu/include/Dialect/TritonCPU/IR/Dialect.h.inc"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 
 #define GET_OP_CLASSES
+#include "npu/include/Dialect/TritonCPU/IR/Dialect.h.inc"
 #include "npu/include/Dialect/TritonCPU/IR/Ops.h.inc"
 
 #endif // TRITON_DIALECT_TRITONCPU_IR_DIALECT_H_
