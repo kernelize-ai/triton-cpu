f41d14875299ee47d353dedcb333f1ff1f8f3622
Alex Baden <alex@kernelize.ai>
2025-09-13 00:08:02 -0400

    Properly set global memory alignment for CPUs

--- backend/compiler.py
+++ backend/compiler.py
@@ -79,6 +79,16 @@ class NPUBackend(BaseBackend):
         npu.load_dialects(ctx)
 
     @staticmethod
+    def parse_attr(desc):
+        ret = []
+        if "D" in desc:
+            ret += [["tt.divisibility", 8]]
+        # pop D from desc
+        desc = desc.replace("D", "")
+        ret += BaseBackend.parse_attr(desc)
+        return ret
+
+    @staticmethod
     def make_ttir(mod, metadata, options):
         pm = ir.pass_manager(mod.context)
         pm.enable_debug()
