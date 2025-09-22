5f1ddd657018cf299ec890d58117eb8f072b772f
Alex Baden <alex@kernelize.ai>
2025-09-22 10:29:55 -0400

    format tt-mlir build script

--- scripts/build-tt-mlir.sh
+++ scripts/build-tt-mlir.sh
@@ -6,7 +6,7 @@ TTMLIR_TOOLCHAIN_DIR="/opt/ttmlir-toolchain/"
 : "${LLVM_BUILD_DIR:?LLVM_BUILD_DIR must be set to the triton LLVM build root directory (typically in ~/.triton/llvm/llvm-OSDISTRO-ARCH)}"
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-# GitHub actions workflows pull triton into the triton-npu plugin directory root 
+# GitHub actions workflows pull triton into the triton-npu plugin directory root
 TRITON_HOME="${TRITON_HOME:-"${REPO_ROOT}/triton"}"
 
 TRITON_VENV_DIR="${TRITON_VENV_DIR:-"$REPO_ROOT/.venv"}"
@@ -19,14 +19,14 @@ cd "$REPO_ROOT/third_party/tt-mlir" || exit 1
 echo "Building tt-mlir env"
 cmake -B env/build env -DTTMLIR_BUILD_LLVM=OFF
 cmake --build env/build
-source env/activate 
+source env/activate
 
 echo "Installing tt-mlir python dependencies"
-python -m pip install nanobind 
+python -m pip install nanobind
 
 #export LLVM_INCLUDE_DIRS="$LLVM_BUILD_DIR/include"
 #export MLIR_INCLUDE_DIRS="$LLVM_BUILD_DIR/include"
-export LLVM_LIBRARY_DIR="$LLVM_BUILD_DIR/lib"
+LLVM_LIBRARY_DIR="$LLVM_BUILD_DIR/lib"
 #export LLVM_SYSPATH="$LLVM_BUILD_DIR"
 MLIR_DIR="$LLVM_LIBRARY_DIR/cmake/mlir"
 LLVM_DIR="$LLVM_LIBRARY_DIR/cmake/llvm"
@@ -38,7 +38,7 @@ if [[ -z "${NO_TTMLIR_RUNTIME:-}" ]]; then
     echo "Building tt-mlir with runtime"
     cmake -G Ninja -B build -DMLIR_DIR="$MLIR_DIR" -DLLVM_DIR="$LLVM_DIR" \
     -DTTMLIR_ENABLE_BINDINGS_PYTHON=OFF -DTTMLIR_ENABLE_RUNTIME=ON -DTT_RUNTIME_ENABLE_TTNN=ON -DTT_RUNTIME_ENABLE_TTMETAL=ON -DTTMLIR_ENABLE_RUNTIME_TESTS=ON
-    
+
     cmake --build build
     cmake --build build -- ttrt
 else
@@ -47,5 +47,3 @@ else
     -DTTMLIR_ENABLE_BINDINGS_PYTHON=OFF -DTTMLIR_ENABLE_RUNTIME=OFF -DTT_RUNTIME_ENABLE_TTNN=OFF -DTT_RUNTIME_ENABLE_TTMETAL=OFF -DTTMLIR_ENABLE_RUNTIME_TESTS=OFF
     cmake --build build
 fi
-
-
