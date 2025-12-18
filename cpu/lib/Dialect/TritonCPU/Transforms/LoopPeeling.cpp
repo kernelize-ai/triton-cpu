04111b6de7e602129e97c70364f0e3b487c36f61
Alex Baden <alex@kernelize.ai>
2025-12-17 20:46:27 -0500

    misc loop peeling code

--- cpu/lib/Dialect/TritonCPU/Transforms/LoopPeeling.cpp
+++ cpu/lib/Dialect/TritonCPU/Transforms/LoopPeeling.cpp
@@ -1,6 +1,9 @@
 #include "cpu/include/Dialect/TritonCPU/Transforms/Passes.h"
 
+#include "mlir/Analysis/SliceAnalysis.h"
 #include "llvm/Support/Debug.h"
+
+#include "triton/Dialect/Triton/Transforms/LoopPeeling.h"
 
 namespace mlir {
 namespace triton {
@@ -15,35 +18,46 @@ namespace cpu {
 
 namespace {
 
-void pruneMasks(DenseSet<Operation*>& candidateMasks) {
-  for (auto it = candidateMasks.begin(); it != candidateMasks.end(); it++) {
-    Operation* maskOp = *it;
-    llvm::errs() << "maskOp = " << *maskOp << "\n";
-
-    // take a backward slice and see if this op depends on the loop induction variable 
-    // TODO: would be really nice if the induction variable was passed to get program id... 
+inline bool programIdOpIsInductionVar(Operation *op) {
+  auto parentLoop = dyn_cast<scf::ForOp>(op->getParentOp());
+  if (parentLoop && parentLoop->hasAttr("program_id_loop")) {
+    return true;
   }
+  return false;
 }
 
-}
+} // namespace
 
 struct LoopPeelingPass : public impl::LoopPeelingPassBase<LoopPeelingPass> {
   using impl::LoopPeelingPassBase<LoopPeelingPass>::LoopPeelingPassBase;
 
   void runOnOperation() override {
     auto moduleOp = getOperation();
-    DenseSet<Operation*> candidateMasks;
-    moduleOp->walk([&](triton::LoadOp loadOp) {
-        if (auto mask = loadOp.getMask()) {
-            LDBG("Evaluating candidate mask for loop peeling: " << mask);
-            candidateMasks.insert(mask.getDefiningOp());
-        }
+    DenseSet<Operation *> candidateMasks;
 
-    pruneMasks(candidateMasks);
+    moduleOp->walk([&](triton::LoadOp loadOp) {
+      if (auto mask = loadOp.getMask()) {
+        LDBG("Evaluating candidate mask for loop peeling: " << mask);
+
+        SetVector<Operation *> slice;
+        (void)getBackwardSlice(mask, &slice);
+
+        if (isa<triton::GetProgramIdOp>(slice.front()) &&
+            programIdOpIsInductionVar(slice.front())) {
+          LDBG("Adding mask to candidate set");
+          candidateMasks.insert(mask.getDefiningOp());
+        }
+      }
     });
+
+    if (!candidateMasks.empty() && candidateMasks.size() == 1) {
+      // peel loop
+      auto forOp = cast<scf::ForOp>((*candidateMasks.begin())->getParentOp());
+      mlir::triton::peelLoopEpilogue(forOp);
+    }
   }
 };
 
-}
-}
-}
+} // namespace cpu
+} // namespace triton
+} // namespace mlir
