629e5fa590c8ff7dd6be00ed7b2c229efc6fac3b
Andrew Brown <andrew.s.brown2@gmail.com>
2026-01-14 13:03:38 -0800

    Add initial implementation of `tt.dot` (#231)
    * Implement a naive conversion for `tt.dot`
    
    * Add a modified version of `03-matrix-multiplication.py`
    
    This has slight differences from the `triton` version:
    - it allows us to distinguish between `cuda`, `hip`, and the new `cpu`
      backend
    - it disables benchmarking (for now)
    
    * Use `TritonGPUToLLVM`'s conversion
    
    This uses `parametericConvertFMADot` to emit the correct packing and
    unpacking operations for the tensor; otherwise, a straightforward
    indexing into the tensor will run into issues with how `TritonGPUToLLVM`
    divides up the tensor.
    
    * Add CI task to run matrix multiplication tutorial
    
    * Remove extra debug macros
    
    * Use `triton::gpu` namespace explicitly
    
    * Extend before multiplying
    
    * Simplify operand size check
    
    * Check that extensions are from smaller bitwidths

---
+++ cpu/lib/TritonCPUToLLVM/DotOpToLLVM.cpp
@@ -0,0 +1,87 @@
+#include "TargetInfo.h"
+
+#include "PatternTritonGPUOpToLLVM.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "triton/Conversion/TritonGPUToLLVM/FMADotUtility.h"
+#include "triton/Conversion/TritonGPUToLLVM/Utility.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "Utility.h"
+
+using namespace mlir;
+using namespace mlir::triton;
+
+namespace {
+
+// Adapted from `TritonGPUToLLVM/DotOpToLLVM/FMA.cpp`.
+class GenericFMAVectorMultiplier : public triton::gpu::FMAVectorMultiplier {
+  OpBuilder &builder;
+  Location loc;
+
+public:
+  GenericFMAVectorMultiplier(OpBuilder &builder, Location loc)
+      : builder(builder), loc(loc) {}
+
+  Value multiplyVectors(ArrayRef<Value> a, ArrayRef<Value> b,
+                        Value c) override {
+    assert(a.size() == b.size() &&
+           "operands to `tt.dot` have mismatched sizes");
+    Value accum = c;
+    Type tgtTy = accum.getType();
+    for (auto it = llvm::zip(a, b).begin(); it != llvm::zip(a, b).end(); ++it) {
+      Value aElem = std::get<0>(*it);
+      Value bElem = std::get<1>(*it);
+
+      // Extend to the target type if needed.
+      const auto ty = aElem.getType();
+      assert(ty == bElem.getType() &&
+             "operands to `tt.dot` have mismatched types");
+      if (ty != tgtTy) {
+        assert(
+            ty.isFloat() && tgtTy.isFloat() &&
+            "only float point type casting is currently supported in `tt.dot`");
+        assert(
+            ty.getIntOrFloatBitWidth() < tgtTy.getIntOrFloatBitWidth() &&
+            "operands to `tt.dot` must be smaller than the accumulator type");
+        aElem = LLVM::FPExtOp::create(builder, loc, tgtTy, aElem);
+        bElem = LLVM::FPExtOp::create(builder, loc, tgtTy, bElem);
+      }
+
+      // Multiply and accumulate.
+      auto mul = LLVM::FMulOp::create(builder, loc, tgtTy, aElem, bElem);
+      accum = LLVM::FAddOp::create(builder, loc, tgtTy, accum, mul);
+    }
+    return accum;
+  }
+};
+
+struct DotOpConversion : public ConvertOpToLLVMPattern<triton::DotOp> {
+  using ConvertOpToLLVMPattern<triton::DotOp>::ConvertOpToLLVMPattern;
+
+  DotOpConversion(LLVMTypeConverter &converter,
+                  const cpu::TargetInfo &targetInfo, PatternBenefit benefit)
+      : ConvertOpToLLVMPattern<triton::DotOp>(converter, benefit),
+        targetInfo(targetInfo) {}
+
+  LogicalResult
+  matchAndRewrite(triton::DotOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto *ctx = rewriter.getContext();
+    auto loc = op.getLoc();
+    GenericFMAVectorMultiplier multiplier(rewriter, loc);
+    return parametricConvertFMADot(op, adaptor, getTypeConverter(), rewriter,
+                                   multiplier);
+  }
+
+private:
+  const cpu::TargetInfo &targetInfo;
+};
+
+} // namespace
+
+void mlir::triton::cpu::populateDotOpToLLVMPatterns(
+    LLVMTypeConverter &typeConverter, RewritePatternSet &patterns,
+    const cpu::TargetInfo &targetInfo, PatternBenefit benefit) {
+  patterns.add<DotOpConversion>(typeConverter, targetInfo, benefit);
+}
