7895deb541057a49533ac936ac1646d80fc1d961
Alex Baden <alex@kernelize.ai>
2026-02-18 16:32:06 -0500

    remove todo

--- backend/driver.py
+++ backend/driver.py
@@ -335,7 +335,6 @@ static PyObject* launch(PyObject* self, PyObject* args) {{
 
   {newline.join(ptr_decls)}
   Py_BEGIN_ALLOW_THREADS;
-  // TODO: parameterize warp size here (add to metadata?)
   _launch(num_warps, shared_memory, gridX, gridY, gridZ, kernel_ptr{', ' + ', '.join(internal_args_list) if len(internal_args_list) > 0 else ''});
   Py_END_ALLOW_THREADS;
   if (PyErr_Occurred()) {{
