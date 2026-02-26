0c2899469edca46e2b40a87d27df056427d8b26d
Alex Baden <alex@kernelize.ai>
2026-02-26 15:50:08 -0500

    Introduce Generic / TileAndFuse pass 2/?

--- cpu/lib/Dialect/TritonCPU/Transforms/TileAndFuse.cpp
+++ cpu/lib/Dialect/TritonCPU/Transforms/TileAndFuse.cpp
@@ -1,5 +1,6 @@
 #include "cpu/include/Dialect/TritonCPU/Transforms/Passes.h"
 
+#include "mlir/Analysis/SliceAnalysis.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "llvm/Support/Debug.h"
 
@@ -16,7 +17,82 @@ namespace cpu {
 #define GEN_PASS_DEF_TRITONCPUTILEANDFUSE
 #include "cpu/include/Dialect/TritonCPU/Transforms/Passes.h.inc"
 
-namespace {}
+namespace {
+
+static bool isClosed(const llvm::SetVector<Operation *> &ops) {
+  llvm::DenseSet<Operation *> set(ops.begin(), ops.end());
+  for (Operation *op : ops) {
+    for (Value r : op->getResults()) {
+      for (Operation *user : r.getUsers()) {
+        if (!set.contains(user))
+          return false;
+      }
+    }
+  }
+  return true;
+}
+
+struct WrapElementwiseChain : public mlir::OpRewritePattern<triton::StoreOp> {
+  using OpRewritePattern<triton::StoreOp>::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(triton::StoreOp storeOp,
+                  mlir::PatternRewriter &rewriter) const override {
+    auto genericParent = storeOp->getParentOfType<cpu::GenericOp>();
+    if (genericParent) {
+      return failure();
+    }
+
+    llvm::SetVector<Operation *> opsToClone;
+    opsToClone.insert(storeOp);
+
+    llvm::SmallVector<Value> queue;
+    queue.push_back(storeOp.getValue());
+
+    bool failed = false;
+    while (!queue.empty()) {
+      auto v = queue.pop_back_val();
+      auto defOp = v.getDefiningOp();
+
+      if (!defOp)
+        continue;
+
+      // allow load operations and elementwise operations in ttc.generic
+      if (auto loadOp = dyn_cast<triton::LoadOp>(defOp)) {
+        // load ops terminate the chain
+        opsToClone.insert(defOp);
+        continue;
+      }
+      if (isa<arith::ArithDialect>(defOp->getDialect()) &&
+          defOp->hasTrait<OpTrait::Elementwise>()) {
+        opsToClone.insert(defOp);
+      } else {
+        failed = true;
+        break;
+      }
+
+      for (auto operand : defOp->getOperands()) {
+        queue.push_back(operand);
+      }
+    }
+    if (failed) {
+      return failure();
+    }
+
+    if (!isClosed(opsToClone)) {
+      return failure();
+    }
+
+    // create generic op using opsToClone as the body. Rewrite load op
+    // parameters to be generic op block args
+    // TODO
+    llvm::errs() << "TODO\n";
+
+    return failure();
+  }
+};
+
+} // namespace
 
 struct TritonCPUTileAndFusePass
     : public impl::TritonCPUTileAndFuseBase<TritonCPUTileAndFusePass> {
@@ -27,6 +103,8 @@ struct TritonCPUTileAndFusePass
     ModuleOp m = getOperation();
     mlir::RewritePatternSet patterns(context);
     constexpr int benefitDefault = 1;
+
+    patterns.add<WrapElementwiseChain>(context, benefitDefault);
 
     if (applyPatternsGreedily(m, std::move(patterns)).failed()) {
       signalPassFailure();
