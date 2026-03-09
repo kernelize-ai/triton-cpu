d07e891b148e292aa2630ada433716c61932d9eb
Alex Baden <alex@kernelize.ai>
2026-03-09 10:03:37 -0400

    add additional verifiers

--- cpu/lib/Dialect/TritonCPU/IR/Ops.cpp
+++ cpu/lib/Dialect/TritonCPU/IR/Ops.cpp
@@ -25,12 +25,26 @@ LogicalResult GenericOp::verify() {
   auto blockShape = getBlockShape();
   auto vectorShape = getVectorShape();
 
+  if (blockShape.size() != 1) {
+    return emitOpError(
+               "only rank-1 generic ops are currently supported, got rank ")
+           << blockShape.size();
+  }
+
   if (blockShape.size() != vectorShape.size()) {
     return emitOpError(
                "expects blockShape and vectorShape to have the same rank, got ")
            << blockShape.size() << " vs " << vectorShape.size();
   }
   for (unsigned i = 0; i < blockShape.size(); i++) {
+    if (blockShape[i] <= 0) {
+      return emitOpError("expects blockShape[")
+             << i << "] to be positive, got " << blockShape[i];
+    }
+    if (vectorShape[i] <= 0) {
+      return emitOpError("expects vectorShape[")
+             << i << "] to be positive, got " << vectorShape[i];
+    }
     if (blockShape[i] < vectorShape[i]) {
       return emitOpError("expects blockShape[")
              << i << "] >= vectorShape[" << i << "], got " << blockShape[i]
