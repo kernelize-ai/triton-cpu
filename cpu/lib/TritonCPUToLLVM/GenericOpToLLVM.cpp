48392a66ca360da6778c568cd2d7d3a8cb491560
Alex Baden <alex@kernelize.ai>
2026-03-18 16:41:21 -0400

    Support generating a runtime loop for tiling generics without tensor arguments

--- cpu/lib/TritonCPUToLLVM/GenericOpToLLVM.cpp
+++ cpu/lib/TritonCPUToLLVM/GenericOpToLLVM.cpp
@@ -131,18 +131,116 @@ struct GenericOpConversion : public ConvertOpToLLVMPattern<cpu::GenericOp> {
     const bool hasReductions = !op.getCombiners().empty();
 
     Block *body = &op.getBody().front();
+
+    const bool hasTensorArgs =
+        llvm::any_of(body->getArguments(), [](BlockArgument arg) {
+          return isa<RankedTensorType>(arg.getType());
+        });
+
     auto b = TritonLLVMOpBuilder(loc, rewriter);
-    // TODO: currently we unroll the generic op during lowering because
-    // extractvalue cannot take a dynamic index. To reduce code size, we will
-    // want to keep some sort of loop here for larger blocks
-    for (unsigned i = 0; i < numChunks; ++i) {
 
-      SmallVector<Value> chunkedArgs =
-          buildStaticChunkedArgs(op, adaptor, rewriter, i, vectorSize);
+    // if the generic op has tensor args we materialize the input tensors then
+    // unroll the loop over tiles, slicing each tensor with the current tile
+    // index. llvm.extractvalue only supports attr indices, so we need to know
+    // the individual tile indices at compile time. However, a generic with no
+    // tensor args can be lowered as a loop which dramatically reduces code
+    // size. Note that generics without tensor args can still materialize
+    // tensors within the body of the generic. If we only have 1 chunk then
+    // there's no need to generate the runtime loop and we "unroll" regardless
+    // of the input type
+    if (hasTensorArgs || numChunks == 1) {
+      for (unsigned i = 0; i < numChunks; ++i) {
 
-      Value chunkOffset = b.i32_val(i * vectorSize);
+        SmallVector<Value> chunkedArgs =
+            buildStaticChunkedArgs(op, adaptor, rewriter, i, vectorSize);
 
-      emitTileBody(op, rewriter, chunkedArgs, chunkOffset, result);
+        Value chunkOffset = b.i32_val(i * vectorSize);
+
+        emitTileBody(op, rewriter, chunkedArgs, chunkOffset, result);
+      }
+    } else {
+      Value one = b.i32_val(1);
+      Value numChunksVal = b.i32_val(numChunks);
+      // peel the first chunk to establish an initial "result" for reductions
+      // TODO  we could only do this if we have reductions but it probably won't
+      // hurt anything
+      auto firstArgs =
+          buildStaticChunkedArgs(op, adaptor, rewriter, 0, vectorSize);
+      emitTileBody(op, rewriter, firstArgs, /*tileOffset=*/b.i32_val(0),
+                   result);
+
+      // build the loop infrastructure for chunks 1...numChunks-1
+      Block *currentBlock = rewriter.getInsertionBlock();
+      Block *afterBlock =
+          rewriter.splitBlock(currentBlock, rewriter.getInsertionPoint());
+
+      Value afterResult;
+      if (hasReductions)
+        afterResult = afterBlock->addArgument(result.getType(), loc);
+
+      SmallVector<Type> headerArgTypes = {i32_ty};
+      SmallVector<Location> headerArgLocs = {loc};
+      if (hasReductions) {
+        headerArgTypes.push_back(result.getType());
+        headerArgLocs.push_back(loc);
+      }
+      Block *loopHeader =
+          rewriter.createBlock(afterBlock, headerArgTypes, headerArgLocs);
+      Block *loopBody = rewriter.createBlock(afterBlock);
+
+      // currentBlock -> loopHeader(1, result)
+      rewriter.setInsertionPointToEnd(currentBlock);
+      {
+        SmallVector<Value> headerInitArgs = {b.i32_val(1)};
+        if (hasReductions)
+          headerInitArgs.push_back(result);
+        LLVM::BrOp::create(rewriter, loc, headerInitArgs, loopHeader);
+      }
+
+      // loopHeader: check bound, branch to body or exit
+      rewriter.setInsertionPointToEnd(loopHeader);
+      Value loopI = loopHeader->getArgument(0);
+      Value loopAcc = hasReductions ? loopHeader->getArgument(1) : Value{};
+      Value cond = LLVM::ICmpOp::create(rewriter, loc, LLVM::ICmpPredicate::ult,
+                                        loopI, numChunksVal);
+      SmallVector<Value> exitArgs;
+      if (hasReductions)
+        exitArgs.push_back(loopAcc);
+      LLVM::CondBrOp::create(rewriter, loc, cond, loopBody, {}, afterBlock,
+                             exitArgs);
+
+      // populate the loop body
+      rewriter.setInsertionPointToEnd(loopBody);
+      {
+        SmallVector<Value> tileArgs;
+        for (auto [opIdx, origArg, llvmArg] : llvm::enumerate(
+                 body->getArguments().drop_front(), adaptor.getOperands())) {
+          assert(!isa<RankedTensorType>(origArg.getType()) &&
+                 "tensor types are not allowed in compile-time generated "
+                 "generic tile loops");
+          // forward the type from the generic body to the loop body
+          assert(isa<PointerType>(origArg.getType()) ||
+                 origArg.getType() == llvmArg.getType() &&
+                     "expected non-tensor arguments to be unchanged by type "
+                     "conversion");
+          tileArgs.push_back(op.getOperand(opIdx));
+        }
+        Value tileResult = loopAcc;
+        Value tileOffset =
+            LLVM::MulOp::create(rewriter, loc, loopI, b.i32_val(vectorSize));
+        emitTileBody(op, rewriter, tileArgs, tileOffset, tileResult);
+
+        Value nextI = LLVM::AddOp::create(rewriter, loc, loopI, one);
+        SmallVector<Value> backArgs = {nextI};
+        if (hasReductions)
+          backArgs.push_back(tileResult);
+        LLVM::BrOp::create(rewriter, loc, backArgs, loopHeader);
+      }
+
+      // forward the final result through the after block and to the next op
+      rewriter.setInsertionPointToStart(afterBlock);
+      if (hasReductions)
+        result = afterResult;
     }
 
     if (result) {
