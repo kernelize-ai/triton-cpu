19a4f24d79ef1f2f6360c8991ef9c333318f463b
Alex Baden <alex.baden@gmail.com>
2026-06-03 14:23:01 -0400

    Support arith::SIToFPOp conversion to LLVM

--- cpu/lib/TritonCPUToLLVM/ElementwiseOpToLLVM.cpp
+++ cpu/lib/TritonCPUToLLVM/ElementwiseOpToLLVM.cpp
@@ -22,6 +22,86 @@ struct FDivOpConversion
   }
 };
 
+static Value checkIsNan(TritonLLVMOpBuilder &builder, Value v) {
+  Location loc = builder.loc;
+  OpBuilder &rewriter = *builder.builder;
+
+  // bits 0 and 1 indicate signaling Nan and quiet Nan, respectively
+  IntegerAttr controlBits = rewriter.getIntegerAttr(i32_ty, 0b11);
+  return LLVM::IsFPClass::create(rewriter, loc, i1_ty, v, controlBits);
+}
+
+static Value convertFp32ToBf16(Location loc,
+                               ConversionPatternRewriter &rewriter,
+                               const Value &v, const RoundingMode rounding) {
+  auto b = TritonLLVMOpBuilder(loc, rewriter);
+  auto as_int32 = b.bitcast(v, i32_ty);
+  if (rounding == RoundingMode::RTZ) {
+    auto shifted = b.lshr(i32_ty, as_int32, b.i32_val(16));
+    auto truncated = b.trunc(i16_ty, shifted);
+    return b.bitcast(truncated, bf16_ty);
+  }
+
+  // This implementation is a faster version for fp32 to bf16 type conversion
+  // It is from CK:
+  // https://github.com/cgmillette/composable_kernel/commit/24e75bef6aa5
+  // It uses less VGPR and less number of instructions compared to the
+  // previous implementation
+  Value isNan = checkIsNan(b, v);
+  Value v16 = b.i32_val(16);
+  Value tmp = b.and_(i32_ty, b.lshr(i32_ty, as_int32, v16), b.i32_val(1));
+
+  Value v7FFF = b.i32_val(0x7FFF);
+  Value s1 = b.add(as_int32, tmp);
+  Value s2 = b.add(s1, v7FFF);
+
+  Value vNan = b.i32_val(0x7FFF0000);
+  Value res = b.select(isNan, vNan, s2);
+
+  Value shifted = b.lshr(i32_ty, res, v16);
+  Value truncated = b.trunc(i16_ty, shifted);
+  return b.bitcast(truncated, bf16_ty);
+}
+
+static SmallVector<Value> S8_to_Bf16(Location loc,
+                                     ConversionPatternRewriter &rewriter,
+                                     const SmallVector<Value> &v) {
+  auto b = TritonLLVMOpBuilder(loc, rewriter);
+  SmallVector<Value> inValues = {v[0], v[1], v[2], v[3]};
+  SmallVector<Value> outValues = {};
+  for (Value inVal : inValues) {
+    Value bf16Val = LLVM::SIToFPOp::create(rewriter, loc, bf16_ty, inVal);
+    outValues.push_back(bf16Val);
+  }
+  return outValues;
+}
+
+struct SIToFPOpConversion
+    : ElementwiseOpConversionBase<arith::SIToFPOp, SIToFPOpConversion> {
+  using ElementwiseOpConversionBase::ElementwiseOpConversionBase;
+
+  SmallVector<Value> createDestOps(arith::SIToFPOp op, OpAdaptor adaptor,
+                                   ConversionPatternRewriter &rewriter,
+                                   Type elemTy, MultipleOperandsRange operands,
+                                   Location loc) const {
+    Type inElemTy = getElementTypeOrSelf(op.getIn());
+    Type outElemTy = getElementTypeOrSelf(op.getOut());
+    if (outElemTy.isBF16() && inElemTy.isInteger(8) && operands.size() >= 4) {
+      SmallVector<Value> inVals = {operands[0][0], operands[1][0],
+                                   operands[2][0], operands[3][0]};
+      auto outVals = S8_to_Bf16(loc, rewriter, inVals);
+      assert(outVals.size() == 4);
+      return outVals;
+    } else if (outElemTy.isBF16()) {
+      auto value =
+          LLVM::SIToFPOp::create(rewriter, loc, f32_ty, operands[0][0]);
+      return {convertFp32ToBf16(loc, rewriter, value, RoundingMode::RTNE)};
+    } else {
+      return {LLVM::SIToFPOp::create(rewriter, loc, elemTy, operands[0][0])};
+    }
+  }
+};
+
 } // namespace
 
 void mlir::triton::cpu::populateElementwiseOpToLLVMPatterns(
@@ -30,6 +110,7 @@ void mlir::triton::cpu::populateElementwiseOpToLLVMPatterns(
     PatternBenefit benefit) {
 
   patterns.add<FDivOpConversion>(typeConverter, axisInfoAnalysis, benefit);
+  patterns.add<SIToFPOpConversion>(typeConverter, axisInfoAnalysis, benefit);
 
   mlir::triton::populateElementwiseOpToLLVMPatterns(
       typeConverter, patterns, axisInfoAnalysis, targetInfo, benefit);
