8b5d112015946e8ee5e512eec5ca85ad5c6dc810
Alex Baden <alex@kernelize.ai>
2026-07-17 20:34:50 -0400

    Properly handle function args for non-kernel triton cpu function calls

--- cpu/lib/TritonCPUToLLVM/PatternTritonGPUOpToLLVM.h
+++ cpu/lib/TritonCPUToLLVM/PatternTritonGPUOpToLLVM.h
@@ -9,6 +9,11 @@ namespace mlir {
 namespace triton {
 namespace cpu {
 
+void populateControlFlowOpToLLVMPattern(LLVMTypeConverter &typeConverter,
+                                        RewritePatternSet &patterns,
+                                        const TargetInfo &targetInfo,
+                                        PatternBenefit benefit);
+
 void populateDotOpToLLVMPatterns(LLVMTypeConverter &typeConverter,
                                  RewritePatternSet &patterns,
                                  const TargetInfo &targetInfo,
